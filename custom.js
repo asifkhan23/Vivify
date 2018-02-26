447c248e3dcfafaa34b61c71d661d39644ac9a1c
Asif Khan <makhan@liquidhub.com>
2018-02-26 17:45:47 +0530

    notes area added

--- custom.js
+++ custom.js
@@ -94,3 +94,38 @@ function hideNews() {
   document.getElementById('hideNews').style.display = "none";
   document.getElementById('newsContainer').style.display = "none";
 }
+
+
+//Text Area Fucntion
+
+(function() {
+
+  function _makeDelayed() {
+    var timer = 0;
+    return function(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  }
+
+  function bindNoteHandlers() {
+    var elem = document.getElementById('note-text'),
+      saveHandler = _makeDelayed();
+
+    function save() {
+      chrome.storage.sync.set({
+        'noteText': elem.value
+      });
+    }
+    // Throttle save so that it only occurs after 1 second without a keypress.
+    elem.addEventListener('keypress', function() {
+      saveHandler(save, 1000);
+    });
+    elem.addEventListener('blur', save);
+    chrome.storage.sync.get('noteText', function(data) {
+      elem.value = data.noteText ? data.noteText : '';
+    });
+  }
+
+  bindNoteHandlers();
+})();
