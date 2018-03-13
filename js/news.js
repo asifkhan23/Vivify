f3e9d571de2d2d59165bbd0c61d5652faf601cd1
prashrey <prashrey04@gmail.com>
2018-03-13 20:34:13 +0530

    minor news changes

--- js/news.js
+++ js/news.js
@@ -5,7 +5,7 @@ var req = new Request(NEWS_API);
 fetch(req)
   .then(data => data.json())
   .then(data => {
-    console.log(data);
+    //console.log(data);
     var newsElem = document.querySelector('.news');
     for (var i = 0; i < 15; i += 3) {
       if (data.articles[i].urlToImage != null) {
@@ -20,15 +20,12 @@ fetch(req)
   })
 
   function getCard(article){
-    var author, titleFull, desc, domCard;
-
-    if (article.title.length > 60) {
-      titleFull = article.title.substring(0, 59) + "...";
+    var author, imgUrl, desc, domCard;
+    imgUrl = article.urlToImage;
+    
+    if(!(article.urlToImage.substring(0, 8) == "https://" || article.urlToImage.substring(0, 7) == "http://")){
+      imgUrl = "https://" + article.urlToImage;
     }
-    else{
-      titleFull = article.title;
-    }
-
     if (article.author == null) {
       author = article.source.name;
     } 
@@ -44,9 +41,9 @@ fetch(req)
     }
 
     domCard = "<div class=\"card\">" +
-      "<img src=" + article.urlToImage + ">" +
-      "<div class=\"title\"><a href=" + article.url + "><span style=\"font-size:2vh\">" + titleFull + "</span></a></div>" +
-      "<div class=\"newsDesc\"><p>" + desc.substr(0, 120) + ".. </p><span>" + article.publishedAt + "</span></div>" +
+      "<img src=" + imgUrl + ">" +
+      "<div class=\"title\"><a href=" + article.url + "><span style=\"font-size:2vh\">" + article.title + "</span></a></div>" +
+      "<div class=\"newsDesc\"><p>" + desc + ".. </p><span>" + article.publishedAt + "</span></div>" +
       "<div class=\"author\"><p>" + author + "</p></div>" +
     "</div>";
     return domCard;
@@ -58,24 +55,20 @@ document.getElementById("collapseNews").addEventListener("click", hideNews);
 
 function displayNews() {
   document.getElementById("newsContainer").style.transition = "width 0.5s";
-  document.getElementById("down").style.transition = "width 0.5s";
   //document.getElementById("newsContainer").style.transitionTimingFunction = "ease-out"
 
   document.getElementById("newsContainerParent").style.width = "100vw";
   document.getElementById("newsContainer").style.width = "100vw";
-  document.getElementById("down").style.width = "100%";
 
   document.getElementById("newsContainer").style.boxShadow = "box-shadow: 1px 1px 5px #555";
 }
 
 function hideNews() {
   document.getElementById("newsContainer").style.transition = "width 0.25s";
-  document.getElementById("down").style.transition = "width 0.25s";
   //document.getElementById("newsContainer").style.transitionTimingFunction = "ease-in"
 
   document.getElementById("newsContainerParent").style.width = "0vw";
   document.getElementById("newsContainer").style.width = "0vw";
-  document.getElementById("down").style.width = "0vw";
 
   document.getElementById("newsContainer").style.boxShadow = "box-shadow: 0px 0px 0px transparent";
 
@@ -83,7 +76,6 @@ function hideNews() {
 
 // Scroll News Tab
 
-document.getElementById("down").addEventListener("mousedown", scrollNews);
 
 function scrollNews() {
   document.getElementsByClassName("newsContainer").scrollTop = +10;
