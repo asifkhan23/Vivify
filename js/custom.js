8af7f06aaf027bf3404a5a8da71a008dfda988a7
Prateek Mittal <mittal.prateek10@gmail.com>
2018-03-21 11:58:00 +0530

    new js file created for variables

--- js/custom.js
+++ js/custom.js
@@ -24,60 +24,6 @@ var beautifulNewTab = (function beautifulNewTab() {
     var weatherLocation = document.getElementById("location");
     var radioQuote = document.querySelector('input[name="radioQuote"]:checked');
 
-    var arrayImg = new Array();
-    arrayImg[0] = "wallpaper1.jpg";
-    arrayImg[1] = "wallpaper2.jpg";
-    arrayImg[2] = "wallpaper3.jpg";
-    arrayImg[3] = "wallpaper4.jpg";
-    arrayImg[4] = "wallpaper5.jpg";
-    arrayImg[5] = "wallpaper6.jpg";
-    arrayImg[6] = "wallpaper7.jpg";
-    arrayImg[7] = "wallpaper8.jpg";
-    arrayImg[8] = "wallpaper9.jpg";
-    arrayImg[9] = "wallpaper10.jpg";
-
-    var famousQuotes = {
-        "quotes": [
-              { "author": "Carol Burnett", "quote": "Only I can change my life. No one can do it for me." },
-              { "author": "Charles R. Swindoll", "quote": "Life is 10% what happens to you and 90% how you react to it." },
-              { "author": "Nikos Kazantzakis", "quote": "In order to succeed, we must first believe that we can." },
-              { "author": "Orison Swett Marden", "quote": "A will finds a way." },
-              { "author": "Nelson Mandela", "quote": "It always seems impossible until it's done." },
-              { "author": "Mark Twain", "quote": "The secret of getting ahead is getting started." },
-              { "author": "Walt Disney", "quote": "If you can dream it, you can do it." },
-              { "author": "Theodore Roosevelt", "quote": "Keep your eyes on the stars, and your feet on the ground." },
-              { "author": "Eleanor Roosevelt", "quote": "With the new day comes new strength and new thoughts." },
-              { "author": "Robert H. Schuller", "quote": "Problems are not stop signs, they are guidelines." },
-              { "author": "Aristotle", "quote": "Quality is not an act, it is a habit." },
-              { "author": "Arthur Ashe", "quote": "Start where you are. Use what you have. Do what you can." },
-              { "author": "Ayn Rand", "quote": "A creative man is motivated by the desire to achieve, not by the desire to beat others." },
-              { "author": "Sam Levenson", "quote": "Don't watch the clock; do what it does. Keep going." },
-              { "author": "Winston Churchill", "quote": "If you're going through hell, keep going." },
-              { "author": "Maya Angelou", "quote": "We may encounter many defeats but we must not be defeated." }
-        ]
-    };
-
-    var moviesQuotes = {
-        "quotes": [
-              { "author": "Love Story", "quote": "Love means never having to say you're sorry." },
-              { "author": "Casablanca", "quote": "Of all the gin joints in all the towns in all the world, she walks into mine." },
-              { "author": "The Godfather", "quote": "I'm going to make him an offer he can't refuse." },
-              { "author": "Star Warsn", "quote": "May the Force be with you." },
-              { "author": "Jaws", "quote": "You're gonna need a bigger boat." },
-              { "author": "Gone With the Wind", "quote": "The first rule of Fight Club is: You do not talk about Fight Club." },
-              { "author": "Fight Club", "quote": "If you can dream it, you can do it." },
-              { "author": "The Dark Knight", "quote": "Why so serious?" },
-              { "author": "When Harry Met Sally", "quote": "I'll have what she's having." },
-              { "author": "Dr. No", "quote": "Bond. James Bond." },
-              { "author": "The Sixth Sense", "quote": "I see dead people." },
-              { "author": "The Terminator", "quote": "I'll be back." },
-              { "author": "A Few Good Men", "quote": "You can't handle the truth!" },
-              { "author": "Toy Story", "quote": "To infinity and beyond!" },
-              { "author": "Dead Poets Society", "quote": "Carpe diem. Seize the day, boys." },
-              { "author": "The Usual Suspects", "quote": "The greatest trick the devil ever pulled was convincing the world he didn't exist." }
-        ]
-    };
-
     document.getElementById("divNoteToggle").addEventListener("click", displayNote);
     document.getElementById("resetDiv").addEventListener("click", resetLocation);
     document.getElementById("saveLocation").addEventListener("click", fetchWeather);
